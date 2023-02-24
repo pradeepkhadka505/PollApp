e1a7ed17bfc648cdfe475b51e40969baca429745
pradeep khadka <pradeepkhadka505@gmail.com>
2023-02-24 12:10:12 +0545

    second update in django poll app

--- PollProject/PollApp/urls.py
+++ PollProject/PollApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.my_poll, name='index')
+    path('', views.index, name='index'),
+    path('getquery', views.getquery, name='getquery')
 ]
--- PollProject/PollApp/views.py
+++ PollProject/PollApp/views.py
@@ -3,5 +3,15 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
-def my_poll(request):
-    return render(request, 'index.html')
+def index(request):
+
+    arr =['java', 'python', 'c','cplusplus','.net','php','javascript','sql','ruby','php','swift','scala']
+    my_dict = {
+        "arr":arr
+    }
+    return render(request, 'index.html', context=my_dict)
+
+def getquery(request):
+    q = request.GET['languages']
+    return HttpResponse(q)
+    
